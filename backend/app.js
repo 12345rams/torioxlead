3df45850ec3a8ddd0e5bfac871de116e7a09f8d2
12345rams <132057037+12345rams@users.noreply.github.com>
2023-09-20 20:33:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ dotenv.config({path:"backend/config/config.env"})
 const errorMiddleware=require('./middleware/error')
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-
+const path=require('path');
 const app= express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +15,10 @@ app.use(express.json())
 const user = require("./routes/userRoute");
 
 app.use("/api/v1", user);
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(path.join(__dirname,"../frontend/build/index.html")))
+})
 app.use(errorMiddleware)
 //Middleware for error
 app.use(errorMiddleware)
