62ed303fee13df6c82369c2b60463529c1df6227
12345rams <132057037+12345rams@users.noreply.github.com>
2023-09-22 12:58:04 +0530

    Add files via upload

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 const connectDatabase=()=>{
-    mongoose.connect("mongodb+srv://bitbytecoders:bitbytecoder123@cluster0.shit8by.mongodb.net/ewaste",{useNewUrlParser: true,
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser: true,
    useUnifiedTopology: true,
     }).then((data)=>{
         console.log(`Mongodb connected with server:${data.connection.host}`)
